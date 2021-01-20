ca7a3520cd119e1568e04e0fbb3f7ee1de096d26
Kshitij Shah <kshitijshah95@gmail.com>
2021-01-20 18:00:23 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+	{
+		title: 'what is your name?',
+		content: 'My name is Kshitij Shah',
+	},
+	{
+		title: 'How old are you?',
+		content: 'I am 25 years old',
+	},
+	{
+		title: 'Where do you live?',
+		content: 'I live in Fullerton, California',
+	},
+];
+
+const options = [
+	{
+		label: 'Hindi',
+		value: 'hi',
+	},
+	{
+		label: 'Arabic',
+		value: 'ar',
+	},
+	{
+		label: 'Afrikaans',
+		value: 'af',
+	},
+];
+
+const App = () => {
+	const [selected, setSelected] = useState(options[0]);
+
+	return (
+		<div>
+			<Header />
+			<Route path='/'>
+				<Accordion items={items} />
+			</Route>
+			<Route path='/search'>
+				<Search />
+			</Route>
+			<Route path='/dropdown'>
+				<Dropdown
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+					label='Select a label'
+				/>
+			</Route>
+			<Route path='/translate'>
+				<Translate />
+			</Route>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+	const [term, setTerm] = useState('programming');
+	const [debouncedTerm, setDebouncedTerm] = useState(term);
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const timeoutId = setTimeout(() => {
+			setDebouncedTerm(term);
+		}, 200);
+
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [term]);
+
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: debouncedTerm,
+				},
+			});
+			setResults(data.query.search);
+		};
+
+		if (debouncedTerm) search();
+	}, [debouncedTerm]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div className='item' key={result.pageid}>
+				<div className='right floated content'>
+					<a
+						className='ui button'
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+						Go
+					</a>
+				</div>
+				<div className='content'>
+					<div className='header'>{result.title}</div>
+					<div dangerouslySetInnerHTML={{ __html: result.snippet }}></div>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div className='ui form'>
+				<div className='field'>
+					<label>Enter Search Term</label>
+					<input
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+						className='input'
+					/>
+				</div>
+			</div>
+			<div className='ui celled list'>{renderedResults}</div>
+		</div>
+	);
+};
+
+export default Search;
